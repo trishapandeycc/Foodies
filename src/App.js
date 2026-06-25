d5d6c873e1e7354dbbec9cb492c83122b34beae7
Trisha Pandey <trisha.pandey.cc@gmail.com>
2026-06-25 18:40:29 +0530

    updated footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import Home from './Components/Home';
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-import Testimonial from './Components/Testimonial';
-import Work from './Components/Work';
-import Footer from "./components/Footer";
+import Testimonial from './Components/testimonial';
+import Work from './Components/work';
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="App">
